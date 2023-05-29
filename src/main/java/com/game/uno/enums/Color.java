b8cfea9a8f15596c0b61d561572489dcddfb6113
Rohit9252 <rohitbatra0165@gmail.com>
2023-05-29 17:53:33 +0530

    first commit

---
+++ src/main/java/com/game/uno/enums/Color.java
@@ -0,0 +1,10 @@
+package com.game.uno.enums;
+
+
+/**
+ * Enumeration representing the colors of the UNO cards.
+ * The enum constants include RED, BLUE, GREEN, YELLOW, and WILD.
+ */
+public enum Color {
+    RED, BLUE, GREEN, YELLOW, WILD
+}
---
+++ src/main/java/com/game/uno/enums/Value.java
@@ -0,0 +1,13 @@
+package com.game.uno.enums;
+
+/**
+ * Enumeration representing the values of the UNO cards.
+ * The enum constants include ZERO to NINE for numbered cards,
+ * SKIP, REVERSE, and DRAW_TWO for special action cards,
+ * and WILD and WILD_DRAW_FOUR for wild cards.
+ */
+public enum Value {
+    ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE,
+    SKIP, REVERSE, DRAW_TWO, WILD, WILD_DRAW_FOUR
+
+}
